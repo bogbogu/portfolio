dd8c21d9bd3cd66f3c22debfbb20662551f7b42c
Benedict Ogbogu <btope0132@gmail.com>
2025-10-17 03:25:51 +0100

    adjusted footer position

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Example: Smooth scroll with jQuery
-$(document).ready(function() {
-  $("a[href^='#']").on("click", function(e) {
+$(document).ready(function () {
+  $("a[href^='#']").on("click", function (e) {
     e.preventDefault();
     const target = $(this.getAttribute("href"));
     if (target.length) {
@@ -18,15 +18,32 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  const currentPage = window.location.pathname.split("/").pop().replace(".html", "");
-  const otherProjects = document.querySelectorAll("#other-projects .project-card");
+  const currentPage = window.location.pathname
+    .split("/")
+    .pop()
+    .replace(".html", "");
+  const otherProjects = document.querySelectorAll(
+    "#other-projects .project-card"
+  );
 
-  otherProjects.forEach(card => {
+  otherProjects.forEach((card) => {
     if (card.dataset.name === currentPage) {
       card.remove();
     }
   });
 });
 
+ const backToTopBtn = document.querySelector('.back-to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      backToTopBtn.classList.add('show');
+    } else {
+      backToTopBtn.classList.remove('show');
+    }
+  });
+
+  backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
